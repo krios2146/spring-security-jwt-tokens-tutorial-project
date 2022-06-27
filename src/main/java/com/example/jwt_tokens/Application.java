c4a8e2f1a6c3090c829348751ff55cca2e3e390f
krios2146 <daun2146@yandex.ru>
2022-06-27 11:58:49 +0700

    Initial commit

--- src/main/java/com/example/jwt_tokens/Application.java
+++ src/main/java/com/example/jwt_tokens/Application.java
@@ -1,4 +1,4 @@
-package com.example.springsecurityjwttokenstutorialproject;
+package com.example.jwt_tokens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/jwt_tokens/ApplicationTests.java
+++ src/test/java/com/example/jwt_tokens/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springsecurityjwttokenstutorialproject;
+package com.example.jwt_tokens;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
